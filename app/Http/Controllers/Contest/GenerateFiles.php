7156b325550442b6da891f73aba46f04ae3952e7
osmanof <oc-inbox@inbox.ru>
2024-09-20 22:53:38 +0300

    Update GenerateFiles.php

--- app/Http/Controllers/Contest/GenerateFiles.php
+++ app/Http/Controllers/Contest/GenerateFiles.php
@@ -53,24 +53,13 @@ class GenerateFiles
     }
 
     public function generatePDFFromTex($inputPath, $outputPath, $title) {
-        $descriptorspec = [
-            1 => ['pipe', 'w'],
-            2 => ['pipe', 'w'],
-        ];
+        $process = new Process(['echo', 'hello']);
 
-        $process = proc_open('echo hello', $descriptorspec, $pipes);
-
-        if (is_resource($process)) {
-            $output = stream_get_contents($pipes[1]);
-            fclose($pipes[1]);
-
-            $error_output = stream_get_contents($pipes[2]);
-            fclose($pipes[2]);
-
-            proc_close($process);
-
-            echo "Output: $output\n";
-            echo "Error: $error_output\n";
+        try {
+            $process->mustRun();
+            \Log::info('PDF generated successfully: ' . $outputPath);
+        } catch (ProcessFailedException $exception) {
+            \Log::error('Error generating PDF: ' . $exception->getMessage());
         }
     }
 
