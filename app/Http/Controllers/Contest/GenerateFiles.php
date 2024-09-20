541ddbe0c6d3cddfbab376af7945272a301fa39b
osmanof <oc-inbox@inbox.ru>
2024-09-20 23:03:44 +0300

    Update GenerateFiles.php

--- app/Http/Controllers/Contest/GenerateFiles.php
+++ app/Http/Controllers/Contest/GenerateFiles.php
@@ -55,6 +55,14 @@ class GenerateFiles
     public function generatePDFFromTex($inputPath, $outputPath, $title) {
         $process = new Process(['~/pdflatex/bin/x86_64-linux/pdflatex', '-interaction=nonstopmode', '-output-directory=' . storage_path("app/private/options"),  $inputPath]);
 
+        $process = new Process([
+            '/home/o/ocinboca/pdflatex/bin/x86_64-linux/pdflatex',
+            '-interaction=nonstopmode',
+            '-output-directory=' . storage_path('app/private/options'),
+            $inputPath
+        ]);
+
+
         try {
             $process->mustRun();
             \Log::info('PDF generated successfully: ' . $outputPath);
